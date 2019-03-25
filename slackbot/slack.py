028372752b0ec354e520d9d2c9d85431dbd4e85a
Glen Giurlanda <Glen.Giurlanda@iptglobal.com>
2019-03-25 16:10:08 -0500

    Implemented notify

--- slackbot/slack.py
+++ slackbot/slack.py
@@ -1,6 +1,5 @@
 import os
-from datetime import datetime
-import calendar
+import time
 from slackclient import SlackClient
 
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
@@ -14,12 +13,15 @@ class SlackAPIClient(object):
     def get_bot_auth_token(self):
         return os.environ.get('SLACK_BOT_TOKEN')
 
-    def send_text_message_to_channel(self, message, channel):
+    def send_message(self, channel, message):
         self.bot_client.api_call(
             "chat.postMessage",
             channel=channel,
             text=message
         )
+
+    def join_channel(self, name):
+        return self.bot_client.api_call("channels.join", name=name)
 
     def get_direct_messages(self):
         return self.bot_client.api_call("im.list")
@@ -35,9 +37,6 @@ class SlackAPIClient(object):
 
     def get_direct_message_history(self, channel):
         return self.bot_client.api_call("conversations.history", channel=channel)
-
-    def mark_message(self, user, time):
-        return self.bot_client.api_call("im.mark", channel=user, ts=time)
 
     def get_all_users(self):
         return self.bot_client.api_call("users.list")
@@ -60,44 +59,60 @@ class SlackAPIClient(object):
         return channel_id
 
     def upload_file_to_channel(self, channel_id, file_url):
-        with open(file_url, errors='ignore') as file_content:
+        with open(file_url, 'rb') as file_content:
             self.bot_client.api_call(
                 "files.upload",
                 channels=channel_id,
                 file=file_content
             )
 
+    def upload_content(self, channel, filename, file):
+        return self.bot_client.api_call("files.upload", channel=channel, filename=filename, file=file)
+
     def upload_file_to_user(self):
         pass
 
+def update():
+    # TODO:
+    #     1) Check reading from device & if reading > threshold display message && hasn't notified channel about this
+    slackClient.send_message(COFFEE_SLACK_CHANNEL, "Wali/Glen Coffee Bot v1.0")
+    return
 
-doods = ['U9FN8T04D', 'U9FNFPJD6', 'U9F5653J5', 'U9FRS7QBU']
+def notify_full():
+    channel = slackClient.get_channel_id(COFFEE_SLACK_CHANNEL)
+    slackClient.upload_file_to_channel(channel, COFFEE_FULL_FILENAME)
+
+def notify_empty():
+    channel = slackClient.get_channel_id(COFFEE_SLACK_CHANNEL)
+    slackClient.upload_file_to_channel(channel, COFFEE_NEEDS_ATTENTION_FILENAME)
+
+
+COFFEE_FULL_THRESHOLD = 50000000
+COFFEE_EMPTY_OR_LOW_THRESHOLD = 0
+COFFEE_FULL_FILENAME = "coffee-full.png"
+COFFEE_NEEDS_ATTENTION_FILENAME="coffee-low.png"
+
+COFFEE_SLACK_CHANNEL = "coffee"
+COFFEE_UPDATE_INTERVAL_IN_MS = 10000
 
 slackClient = SlackAPIClient()
 
+while True:
+    #
+    # read scale, if reading is empty or full, send notification.
+    #
+    # var weight = get_weight();
+    # if (IsLow(weight) && is_high)     if is_high initial value is true
+    #    is_high = false;
+    #    is_low = true
+    #    notify_empty();
+    #
+    # if (IsHigh(weight) && is_low)
+    #    is_low = false;
+    #    is_high = true;
+    #    notify_full();
+    #
 
 
-conversations = slackClient.get_direct_messages()
 
-for conversation in conversations["ims"]:
-    dmId = conversation["id"]
-    convo = slackClient.get_unread_conversations(dmId)
-    z = slackClient.get_unread_conversations(conversation["id"])
-    count = z["unread_count_display"]
-
-    if count > 0:
-        d = datetime.utcnow()
-        unixtime = calendar.timegm(d.utctimetuple())
-        aa = slackClient.mark_message(dmId, unixtime)
-        print("")
-
-    z2 = slackClient.get_unread_conversations(conversation["id"])
-    count2 = z["unread_count_display"]
-    print("")
-
-print("hello")
-
-
-# slackClient = SlackAPIClient()
-# channelId = slackClient.get_channel_id("doods")
-# slackClient.send_text_message_to_channel("yoooo", channelId)
+    time.sleep(COFFEE_UPDATE_INTERVAL_IN_MS)
